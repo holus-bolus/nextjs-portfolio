b3a2d52a6281f4c20a71f3a0d8ce757cbfca7d7f
holus-bolus <ustinovoleksij@gmail.com>
2024-04-06 11:43:33 +0300

    Added pages

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+return (
+<div>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Components</li>
+        </ul>
+    </nav>
+</div>);
+};
+
+export default Navbar;
