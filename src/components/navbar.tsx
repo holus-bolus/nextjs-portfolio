2a4697958c044a74dc742cec40c7a88e9db46934
holus-bolus <ustinovoleksij@gmail.com>
2024-04-06 12:16:24 +0300

    Added the burger menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
 
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
 const Navbar = () => {
   return (
     <div
@@ -23,6 +30,22 @@ const Navbar = () => {
             .dev
           </span>
         </Link>
+      </div>
+      <div>
+        <button
+          className={"w-10 h-8 flex flex-col justify-between z-50 relative"}
+        >
+          <div className={"w-10 h-1 bg-white rounded"}></div>
+          <div className={"w-10 h-1 bg-white rounded"}></div>
+          <div className={"w-10 h-1 bg-white rounded"}></div>
+        </button>
+        <ul className="menu-items absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl">
+          {links.map((link) => (
+            <Link key={uuidv4()} href={link.url}>
+              {link.title}
+            </Link>
+          ))}
+        </ul>
       </div>
     </div>
   );
