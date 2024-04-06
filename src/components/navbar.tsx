6e248907b288d9cec61a2b34030d2f30c0ff21a8
holus-bolus <ustinovoleksij@gmail.com>
2024-04-06 11:59:46 +0300

    Added the header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,31 @@
+import Link from "next/link";
+
 const Navbar = () => {
-return (
-<div>
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Components</li>
-        </ul>
-    </nav>
-</div>);
+  return (
+    <div
+      className={
+        "h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48"
+      }
+    >
+      <div>
+        <Link
+          href={"/"}
+          className={
+            "text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+          }
+        >
+          <span className={"text-white mr-1"}>holus-bolus</span>
+          <span
+            className={
+              "w-12 rounded bg-white text-black flex items-center justify-center"
+            }
+          >
+            .dev
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
